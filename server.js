343048d0327c934c78c6a15166b3f8241327817e
kiayyy375 <kiayyy375@gmail.com>
2026-06-21 18:21:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -242,7 +242,9 @@ if (currentProcess >= MAX_PROCESS) {
 currentProcess++
 
 
-        const perintahFfmpeg = `ffmpeg -loglevel panic -i "${file.path}" -vf "scale='if(gte(iw,ih),-2,720)':'if(gte(iw,ih),720,-2)',hqdn3d=0.5:0.5:1.0:1.0,unsharp=3:3:0.3:3:3:0.3" -r ${targetFps} -c:v libx264 -preset superfast -crf 18 -aq-mode 3 -colorspace bt709 -color_trc bt709 -color_primaries bt709 -maxrate 8M -bufsize 8M -pix_fmt yuv420p "${normalized}"`
+        
+
+        const perintahFfmpeg = `ffmpeg -i "${file.path}" -vf "scale=ih*720/iw:720:force_original_aspect_ratio=decrease,pad=720:720:(ow-iw)/2:(oh-ih)/2,hqdn3d=0.5:0.5:1.0:1.0,unsharp=3:3:0.3:3:3:0.3" -r ${targetFps} -c:v libx264 -preset superfast -crf 18 -colorspace bt709 -color_trc bt709 -color_primaries bt709 -maxrate 8M -bufsize 8M -pix_fmt yuv420p "${normalized}"`
 
         const videoId = `vid_${Date.now()}`
         global.videoProgress[videoId] = { status: "proses", message: "Sedang mengompres video jadi HD..." }
